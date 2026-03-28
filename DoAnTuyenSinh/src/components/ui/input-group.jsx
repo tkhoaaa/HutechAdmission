bd3f91b8e43e64c2f67ff614bd6365a14ba9e529
tkhoaaa <khoa0372243036@gmail.com>
2026-03-28 14:15:08 +0700

    Update

--- DoAnTuyenSinh/src/components/ui/input-group.jsx
+++ DoAnTuyenSinh/src/components/ui/input-group.jsx
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/Button"
 import { Input } from "@/components/ui/Input"
-import { Textarea } from "@/components/ui/Textarea"
+import { Textarea } from "@/components/ui/textarea"
 
 function InputGroup({
   className,
